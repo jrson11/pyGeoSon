2c04a7251ad2fbdc01159707171c733555e895af
J.Son <91623332+jrson11@users.noreply.github.com>
2023-01-02 15:47:16 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@
 #
 # Licensed under the ???
 
+import os
 from setuptools import setup, find_packages
 
+# Info
+VERSION='0.0.1'
+AUTHORS='Jungrak Son, Yichuan Zhu'
+DESCRIPTION='A Python Library for Geotechnical Engineering'
 
 setup(
     name="pyGeoSon",
-    version='0.0.1',
-    authour="Jungrak Son, Yichuan Zhu"
+    version=VERSION,
+    authour=AUTHORS,
     author_email="jon.jungrak.son@gmail.com",
-    description="A Python Library for Geotechnical Engineering",
+    description=DESCRIPTION,
     license="GPL, Version 2.0 ???",
     url="https://github.com/jrson11/pyGeoSon.git ???"
     #
     packages=find_packages()
+    install_requires=[]
+    keywords=['python','civil','geotechnical','engineering','geotechnics']
     include_package_data=True,
     python_requires='>=3.6',
 )
