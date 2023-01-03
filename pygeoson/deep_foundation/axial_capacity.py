01cfb199286a4447be6f56c63c4640da6ad16604
J.Son <91623332+jrson11@users.noreply.github.com>
2023-01-03 08:25:53 -0600

    Create axial_capacity.py

---
+++ pygeoson/deep_foundation/axial_capacity.py
@@ -0,0 +1,16 @@
+# Name: axial_capacity.py
+# Purpose: To calculate pile bearing capacity
+# Authour: Jungrak Son
+# Version: 1/3/2023
+
+import numpy as np
+import pandas as pd
+
+class axial_capacity_FHWA():
+  
+  def __init__(self):
+    self = 'test'
+    
+  def ground_model():
+    a = 'test'
+    return a
