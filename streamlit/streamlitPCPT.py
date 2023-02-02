21d396253707374af5b6e4931d6f14c117e85940
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 08:46:27 -0600

    Update streamlitPCPT.py

--- streamlit/streamlitPCPT.py
+++ streamlit/streamlitPCPT.py
@@ -5,3 +5,6 @@ import altair as alt
 
 
 from groundhog.general.soilprofile import SoilProfile, profile_from_dataframe
+
+st.set_page_config(page_title="Groundhog PCPT processing",layout='wide')
+cols = st.beta_columns([1, 3])
