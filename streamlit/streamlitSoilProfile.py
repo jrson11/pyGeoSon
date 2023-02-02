60964dea7d579e0f6e4bc8e2198b7f2b4d575c44
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 09:36:24 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -1,19 +1,17 @@
 import streamlit as st
 
-'''
-Front-End =============================================
-  - Purpose: to develop user friendly GUI of engineering web app
-  - Author: J.Chang (000@gmail.com)
-  - Last update:
-'''
+
+# Front-End =============================================
+#  - Purpose: to develop user friendly GUI of engineering web app
+#  - Author: J.Chang (000@gmail.com)
+#  - Last update:
 st.markdown('## Soil Profile')
 
-'''
-Back-End =============================================
-  - Purpose: to deploy geo engineering skills
-  - Author: J.Son (jon.jungrak.son@gmail.com)
-  - Last update: 2/2/2023
-'''
+
+# Back-End =============================================
+# - Purpose: to deploy geo engineering skills
+#  - Author: J.Son (jon.jungrak.son@gmail.com)
+#  - Last update: 2/2/2023
 
 ## Initialization
 from groundhog.general import soilprofile as sp
