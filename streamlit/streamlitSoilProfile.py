fd43bf58c8750e82a4c5335d3160eb3dc6fa1e2f
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 13:46:33 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -31,7 +31,14 @@ profile_2 = sp.SoilProfile({
     'Total unit weight [kN/m3]': [19, 18, 19, 20]
 })
 
-
+fig = make_subplots(rows=1, cols=2)
+fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth from [m]'],
+                         mode='markers', name='UW'), row=1,col=1)
+fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth to [m]'],
+                         mode='markers', name='UW'), row=1,col=1)
+#
+fig.update_yaxes(autorange="reversed")
+st.plotly_chart(fig)
 
 
 '''
