7f703c35657842b94fa97fb1dd86e3a77e1157cf
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 13:37:41 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -41,37 +41,17 @@ fig = profile_2.plot_profile(
     fillcolordict={'SAND': 'yellow', 'CLAY': 'brown', 'SILT': 'green'})
 '''
 
-'''
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([1, 3, 2, 3, 1])
-
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=x, y=y, name="linear",
-                    line_shape='linear'))
-fig.add_trace(go.Scatter(x=x, y=y + 5, name="spline",
-                    text=["tweak line smoothness<br>with 'smoothing' in line object"],
-                    hoverinfo='text+name',
-                    line_shape='spline'))
-fig.add_trace(go.Scatter(x=x, y=y + 10, name="vhv",
-                    line_shape='vhv'))
-fig.add_trace(go.Scatter(x=x, y=y + 15, name="hvh",
-                    line_shape='hvh'))
-fig.add_trace(go.Scatter(x=x, y=y + 20, name="vh",
-                    line_shape='vh'))
-fig.add_trace(go.Scatter(x=x, y=y + 25, name="hv",
-                    line_shape='hv'))
-
-fig.update_traces(hoverinfo='text+name', mode='lines+markers')
-fig.update_layout(legend=dict(y=0.5, traceorder='reversed', font_size=16))
-
-fig = go.Figure()
+#
 fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth to [m]'], line_shape='hv', name='hv'))
 fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth to [m]'], line_shape='vh', name='vh'))
 fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth from [m]'], line_shape='hv', name='hv2'))
 fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth from [m]'], line_shape='vh', name='vh2'))
 fig.update_yaxes(autorange="reversed")
 fig.update_layout(autosize=False,width=400,height=700)
-'''
+#
+st.plotly_chart(fig)
+
 
 fig = make_subplots(rows=1, cols=2)
 #
@@ -82,6 +62,5 @@ fig.add_trace(go.Scatter(x=profile_2['qc from [MPa]'], y=profile_2['Depth from [
 #
 fig.update_yaxes(autorange="reversed")
 fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
-
-
+#
 st.plotly_chart(fig)
