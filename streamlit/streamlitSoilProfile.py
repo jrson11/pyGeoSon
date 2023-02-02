2da27828e842311ae029d6b63dd7c9f9e0dc78bc
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 13:36:35 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -81,7 +81,6 @@ fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['
 fig.add_trace(go.Scatter(x=profile_2['qc from [MPa]'], y=profile_2['Depth from [m]'], line_shape='linear', name='qc'), row=1,col=2)
 #
 fig.update_yaxes(autorange="reversed")
-fig.update_layout(yaxis2=[0,10])
 fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
 
 
