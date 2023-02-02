4970ae1271b7c5747e9673e9a737c9dba9134969
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 09:35:12 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
 '''
-Front-End
+Front-End =============================================
+  - Purpose: to develop user friendly GUI of engineering web app
+  - Author: J.Chang (000@gmail.com)
+  - Last update:
 '''
 st.markdown('## Soil Profile')
 
-## Back-End
+'''
+Back-End =============================================
+  - Purpose: to deploy geo engineering skills
+  - Author: J.Son (jon.jungrak.son@gmail.com)
+  - Last update: 2/2/2023
+'''
