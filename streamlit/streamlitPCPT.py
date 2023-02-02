e9a6dca339e286905d4548fc330e51461f44f3d6
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 08:41:44 -0600

    Update streamlitPCPT.py

--- streamlit/streamlitPCPT.py
+++ streamlit/streamlitPCPT.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from copy import deepcopy
-
-#def streamlitPCPT():
-from groundhog.siteinvestigation.insitutests.pcpt_processing import PCPTProcessing, DEFAULT_CONE_PROPERTIES
-from groundhog.general.soilprofile import SoilProfile, profile_from_dataframe
+import numpy as np
+import pandas as pd
+import altair as alt
