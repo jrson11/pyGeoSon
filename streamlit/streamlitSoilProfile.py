80e107b03698bf3846416d27631fb37b5e20b8a0
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 13:55:55 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -35,9 +35,9 @@ fig = make_subplots(rows=1, cols=3, subplot_titles=('Log','UW','CPT'))
 #
 #
 fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth from [m]'],
-                         mode='lines', line_shape='vh'), row=1,col=2)
+                         mode='lines+markers', line_shape='vh'), row=1,col=2)
 fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth to [m]'],
-                         mode='markers', name='UW'), row=1,col=2)
+                         mode='lines+markers', line_shape='vh'), row=1,col=2)
 #
 fig.add_trace(go.Scatter(x=profile_2['qt [MPa]'], y=profile_2['Depth to [m]'],
                          mode='lines+markers', name='qc'), row=1,col=3)
