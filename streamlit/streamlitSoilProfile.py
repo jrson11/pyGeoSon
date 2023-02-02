d35f9d0616b08dcc8a0b4df09a3c80e0b41ec61c
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 13:12:17 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -63,7 +63,7 @@ fig.update_traces(hoverinfo='text+name', mode='lines+markers')
 fig.update_layout(legend=dict(y=0.5, traceorder='reversed', font_size=16))
 '''
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth to [m]']))
+fig.add_trace(go.Scatter(x=profile_2['Total unit weight [kN/m3]'], y=profile_2['Depth to [m]']), line_shape='hv')
 
 
 st.plotly_chart(fig)
