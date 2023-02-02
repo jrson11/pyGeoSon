c9af1362aa6e11c376e8008db359ccd71ecb9d4b
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-02 09:48:45 -0600

    Update streamlitSoilProfile.py

--- streamlit/streamlitSoilProfile.py
+++ streamlit/streamlitSoilProfile.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+import plotly.figure_factory as ff
 import plotly.graph_objs as go
 from plotly.offline import init_notebook_mode, iplot, download_plotlyjs
 from groundhog.general import soilprofile as sp
@@ -26,7 +28,9 @@ profile_2 = sp.SoilProfile({
     'qc to [MPa]': [4, 1.5, 8, 50],
     'qt [MPa]': [3.5, 1.25, 6, 45],
     'Total unit weight [kN/m3]': [19, 18, 19, 20]
-  
+    
+    
+'''  
 fig = profile_2.plot_profile(
     parameters=(('Total unit weight [kN/m3]',), ('qc [MPa]', 'qt [MPa]')),
     showlegends=((False,), (True, True)),
@@ -35,6 +39,12 @@ fig = profile_2.plot_profile(
     xranges=((15, 22), (0, 60)),
     zrange=(10, 0),
     fillcolordict={'SAND': 'yellow', 'CLAY': 'brown', 'SILT': 'green'})
+'''
+    
+ # Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
     
 st.plotly_chart(fig, use_container_width=True)
 
