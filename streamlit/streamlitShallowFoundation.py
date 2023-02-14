fb35bcc8ad80203cc5059954584e6ee2c716e99b
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-14 16:16:50 -0600

    Update streamlitShallowFoundation.py

--- streamlit/streamlitShallowFoundation.py
+++ streamlit/streamlitShallowFoundation.py
@@ -1,23 +1,16 @@
-# Contents of ~/my_app/streamlit_app.py
 import streamlit as st
 
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+st.markdown(
+    """
+    Shallow Foundation Analysis with pyGeoSon
+    **👈 Select ** adsf
+    ### Why?
+    - 
+    ### What?
+    -
+    """
+)
 
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
+st.sidebar.success("Select a demo")
 
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+st.write("# Welcome to Streamlit! 👋")
