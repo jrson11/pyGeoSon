ff804b0e157d53abdbcb84e801e6a6cb9cbcad3f
J.Son <91623332+jrson11@users.noreply.github.com>
2023-02-14 16:02:31 -0600

    Update streamlitShallowFoundation.py

--- streamlit/streamlitShallowFoundation.py
+++ streamlit/streamlitShallowFoundation.py
@@ -16,7 +16,7 @@ def page3():
 page_names_to_funcs = {
     "Main Page": main_page,
     "Page 2": page2,
-    "Page 3": page3,
+    "Page 3": page3
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
